7dacbc3af6ab32348f7b6fe7b3a4586aa361ddec
webtech <webtech@webtech-debian.webtech>
2023-06-10 15:57:13 -0400

    change home and tweets paths

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "tweets#index"
+  root "application#home"
   resources :tweets, except: [:update, :edit]
 end
