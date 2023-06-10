34fa2664e29096c2357f6a35a6aa946526a61915
webtech <webtech@webtech-debian.webtech>
2023-06-10 16:14:29 -0400

    feat:tweet new & create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,9 +13,18 @@ class TweetsController < ApplicationController
     end
     
     def create
+        tweet = Tweet.new(tweet_params)
+        tweet.user = current_user
+        if tweet.save
+            redirect_to tweet, notice: "Tweet succesful"
+        else
+            render :new
+        end
+
     end
 
+    private
     def tweet_params
-        params.require(:tweet).permit(:body, :user_id)
+        params.require(:tweet).permit(:body)
     end
 end
