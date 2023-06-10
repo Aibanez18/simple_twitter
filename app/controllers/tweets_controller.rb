03b9e4c215fdedcfd97375c69fe9890870c26d33
webtech <webtech@webtech-debian.webtech>
2023-06-10 15:47:31 -0400

    feat:tweet#index

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,22 @@
+class TweetsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @tweets = Tweet.all
+    end
+
+    def show
+        @tweet = Tweet.find(params[:id])
+    end
+
+    def new
+        @tweet = Tweet.new
+    end
+    
+    def create
+    end
+
+    def tweet_params
+        params.require(:tweet).permit(:body, :user_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "application#home"
+  root "tweets#index"
+  resources :tweets, except: [:update, :edit]
 end
